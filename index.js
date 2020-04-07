eee7d2d8d572f66f8cc616d2cee6697290bc5cd9
Ronen Rokhit <r.rokhit11@gmail.com>
2020-04-06 21:53:35 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,152 @@
+//Guap Software
+const electron = require("electron");
+const path = require("path");
+const BrowserWindow = electron.remote.BrowserWindow;
+var inputvalue = document.getElementById("date-selector");
+var radioResponse = document.getElementsByName("initial-menu");
+// var getDivision = document.getElementByName("initial-menu").value;
+var isChecked = document.querySelector("input[name=initial-menu]:checked");
+var documents = document.getElementById("documents");
+var dateName = document.getElementById("date-name");
+var date;
+var docType;
+
+//Document list for IRB documents
+documentListIRB = [
+  "Please Select:",
+  "Disclosure (IAD)",
+  "Notice of Appeal (IAD)",
+  "Disclosure (RPD)",
+  "Notice of Appeal (RAD)",
+  "Appeal Record"
+];
+
+//Document list for FC documents
+documentListFC = [
+  "Please Select:",
+  "Application for Leave and Judicial Review (Domestic)",
+  "Application for Leave and Judicial Review (International)",
+  "Applicant's Record",
+  "Affidavits and Memorandum",
+  "Reply Memorandum",
+  "Book of Authorities"
+];
+
+//Gets date from date box
+function getDate() {
+  var dateControl = document.querySelector('input[type="date"]');
+  date = dateControl.value;
+  console.log(date);
+}
+
+//Shows FC documents in drop-down menu
+function documentsFC() {
+  var len = documents.length;
+  for (let i = 0; i < len; i++) {
+    documents.removeChild(documents.options[0]);
+  }
+  for (let i = 0; i < documentListFC.length; i++) {
+    var opt = document.createElement("option");
+    opt.appendChild(document.createTextNode(documentListFC[i]));
+    opt.value = documentListFC[i];
+    documents.appendChild(opt);
+  }
+}
+
+//Shows IRB documents in drop-down menu
+function documentsIRB() {
+  var len = documents.length;
+  for (let i = 0; i < len; i++) {
+    documents.removeChild(documents.options[0]);
+  }
+
+  for (let i = 0; i < documentListIRB.length; i++) {
+    var opt = document.createElement("option");
+    opt.appendChild(document.createTextNode(documentListIRB[i]));
+    opt.value = documentListIRB[i];
+    documents.appendChild(opt);
+  }
+}
+
+//Dynamically changes drop-down menu to IRB or FC documents
+for (i = 0; i < radioResponse.length; i++) {
+  radioResponse[i].addEventListener("change", function(event) {
+    if (this.value == "IRB") {
+      documentsIRB();
+    } else {
+      documentsFC();
+    }
+  });
+}
+
+//what to do if docType = something
+function disclosure() {
+  dateName.innerHTML = "Date of hearing: ";
+}
+
+function noticeOfAppeal() {
+  dateName.innerHTML = "Date received: ";
+}
+
+function applicationFLJR() {
+  dateName.innerHTML = "Date received ";
+}
+
+function applicationRecord() {
+  dateName.innerHTML = "Date AFLJR sent/Rule 9 received: ";
+}
+
+function replyMemo() {
+  dateName.innerHTML = "Date FC memo received: ";
+}
+
+function appealRecord(){
+  dateName.innerHTML = "Date notice of appeal was filed: "
+}
+
+//Saves value of DocType
+documents.addEventListener("change", function(event) {
+  docType = event.target.value;
+  console.log(docType);
+  if (docType == "Disclosure (IAD)" || docType == "Disclosure (RPD)") {
+    console.log(docType);
+    disclosure();
+  } else if (
+    docType == "Notice of Appeal (IAD)" ||
+    docType == "Notice of Appeal (RAD)"
+  ) {
+    noticeOfAppeal();
+  } else if (
+    docType == "Application for Leave and Judicial Review (Domestic)" ||
+    docType == "Application for Leave and Judicial Review (International)"
+  ) {
+    applicationFLJR();
+  } else if (docType == "Applicant's Record") {
+    applicationRecord();
+  } else if (
+    docType == "Reply Memorandum" ||
+    docType == "Affidavits and Memorandum"
+  ) {
+    replyMemo();
+  }else if(docType == "Appeal Record"){
+    appealRecord();
+  }
+});
+
+inputvalue.addEventListener("change", function(event) {
+  date = inputvalue.value;
+  console.log(date);
+  // document.getElementById("date-form").submit();
+});
+
+//date: date
+//document type: docType
+//Calculate due date of...
+
+valuesIRB = {
+  "Disclosure (IAD)": 20,
+  "Notice of Appeal (IAD)": 30,
+  "Disclosure (RPD)": 10,
+  "Notice of Appeal (RAD)": 15
+};
+
