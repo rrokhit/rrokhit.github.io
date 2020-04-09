3fc5360495c50f5becc4d78132819d067c5477c0
Ronen Rokhit <39106199+rrokhit@users.noreply.github.com>
2020-04-08 22:06:23 -0400

    Add files via upload

---
+++ graphConfig.js
@@ -0,0 +1,23 @@
+const graphConfig = {
+  graphMeEndpoint: "https://graph.microsoft.comv1.0/me",
+  graphMailEndpoint: "https://graph.microsoft.comv1.0/me/messages",
+};
+
+function callMSGraph(endpoint, token, callback) {
+  const headers = new Headers();
+  const bearer = `Bearer ${token}`;
+
+  headers.append("Authorization", bearer);
+
+  const options = {
+    method: "GET",
+    headers: headers,
+  };
+
+  console.log("request made to Graph API at: " + new Date().toString());
+
+  fetch(endpoint, options)
+    .then((response) => response.json())
+    .then((response) => callback(response, endpoint))
+    .catch((error) => console.log(error));
+}
