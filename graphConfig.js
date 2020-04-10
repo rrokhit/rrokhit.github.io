04f9180a8aa061dae5fbb7b9c0260de1f8a3ad66
Ronen Rokhit <39106199+rrokhit@users.noreply.github.com>
2020-04-09 22:08:35 -0400

    Update graphConfig.js

--- graphConfig.js
+++ graphConfig.js
@@ -1,7 +1,42 @@
 const graphConfig = {
   graphMeEndpoint: "https://graph.microsoft.com/v1.0/me",
   graphMailEndpoint: "https://graph.microsoft.com/v1.0/me/messages",
+  graphCalendarEndpointOne: "https://graph.microsoft.com/v1.0/me/calendar/events",
+  graphCalendarEndpointTwo: "https://graph.microsoft.com/v1.0/users/{id | userPrincipalName}/calendar/events",
+  graphCalendarEndpointThree: "https://graph.microsoft.com/v1.0/groups/{id}/calendar/events"
+
 };
+
+
+
+const event = {
+  subject: "Let's go for lunch",
+  body: {
+    contentType: "HTML",
+    content: "Does mid month work for you?",
+  },
+  start: {
+    dateTime: "2019-03-15T12:00:00",
+    timeZone: "Pacific Standard Time",
+  },
+  end: {
+    dateTime: "2019-03-15T14:00:00",
+    timeZone: "Pacific Standard Time",
+  },
+  location: {
+    displayName: "Harry's Bar",
+  },
+  attendees: [
+    {
+      emailAddress: {
+        address: "adelev@contoso.onmicrosoft.com",
+        name: "Adele Vance",
+      },
+      type: "required",
+    },
+  ],
+};
+
 
 // function callMSGraph(endpoint, token) {
 //   const headers = new Headers();
@@ -30,10 +65,32 @@ function callMSGraph(theUrl, accessToken){
   var xmlHttp = new XMLHttpRequest();
   xmlHttp.onreadystatechange = function(){
     if(this.readyState == 4 && this.status == 200){
-      console.log(xmlHttp.responseText);
+      return(xmlHttp.responseText);
     }
   }
   xmlHttp.open("GET", theUrl, true);
   xmlHttp.setRequestHeader('Authorization','Bearer ' + accessToken);
   xmlHttp.send();
 }
+
+
+function callMSGraph2(theUrl, token) {
+
+  // const options = {
+  //   authProvider,
+  // }
+
+  // const client = Client.init(options);
+  var xmlHttp = new.xmlHttpRequest();
+  xmlHttp.onreadystatechange = function () {
+    if (this.readystate == 4 && this.status == 201) {
+      console.log(xmlHttp.responseText);
+    }
+  };
+
+  xmlHttp.open("POST", theUrl, true);
+  xmlHttp.setRequestHeader("Authorization", "Bearer " + token);
+  xmlHttp.setRequestHeader("Content-Type", "application/json");
+  xmlHttp.send(event);
+}
+
