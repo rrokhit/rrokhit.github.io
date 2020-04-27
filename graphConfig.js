871be62aa98fc4e1c64376b2a110313f6724afd0
Ronen Rokhit <39106199+rrokhit@users.noreply.github.com>
2020-04-27 13:58:41 -0400

    Update graphConfig.js

--- graphConfig.js
+++ graphConfig.js
@@ -1,37 +1,32 @@
-var subjectholder = document.getElementById("event-subject"); 
+var subjectholder = document.getElementById("event-subject");
 var clientName = document.getElementById("client-name");
 var datalist = document.getElementById("attendees");
-var datalistValue = document.getElementById("attendees-search")
+var datalistValue = document.getElementById("attendees-search");
 var body = document.getElementById("event-content");
 var contactResponse;
 
-
-
-
 //subject line for calendar event
-clientName.addEventListener("input",function(event){
+clientName.addEventListener("input", function (event) {
   selIndex = docType.selectedIndex;
   console.log(docType);
   console.log(clientName.value);
   var subjectText = clientName.value + " " + docType;
   subjectholder.value = subjectText;
-
-})
-
+});
 
 //endpoints for various API calls
 const graphConfig = {
   graphMeEndpoint: "https://graph.microsoft.com/v1.0/me",
   graphMailEndpoint: "https://graph.microsoft.com/v1.0/me/messages",
-  graphCalendarEndpointOne: "https://graph.microsoft.com/v1.0/me/calendar/events",
- graphPeopleEndpoint : "https://graph.microsoft.com/v1.0/me/people",
- graphContactsEndpoint : "https://graph.microsoft.com/v1.0/me/contacts?$select = displayName,emailAddresses"
+  graphCalendarEndpointOne:
+    "https://graph.microsoft.com/v1.0/me/calendar/events",
+  graphPeopleEndpoint: "https://graph.microsoft.com/v1.0/me/people",
+  graphContactsEndpoint:
+    "https://graph.microsoft.com/v1.0/me/contacts?$select = displayName,emailAddresses",
 };
 
-
-
-//event template 
-const event ={
+//event template
+const event = {
   subject: subjectholder.value,
   reminderMinutesBeforeStart: "",
   body: {
@@ -49,99 +44,102 @@ const event ={
   location: {
     displayName: null,
   },
-  attendees: [{
-    emailAddress : {
-      address : "kwvembu@gmail.com",
-      name: "Vembushri RM"
-    }, 
-    type: "required"
-  }]
+  attendees: [
+    {
+      emailAddress: {
+        address: "kwvembu@gmail.com",
+        name: "Vembushri RM",
+      },
+      type: "required",
+    },
+  ],
 };
 
 //append attendees to this array and then set the event attendees to this value
 attendeesForEvent = [
   //this is the default attendee for each event
-//   {emailAddress: {
-//   address : "cdeslosges@desloges.ca",
-//   name: "Chantal Desloges"
-// },
-// type: "required"
-// },
-  {emailAddress: {
-  address : "r.rokhit11@gmail.com",
-  name: "Ronen LeRokh"
-},
-type: "required"
-}
+  //   {emailAddress: {
+  //   address : "cdeslosges@desloges.ca",
+  //   name: "Chantal Desloges"
+  // },
+  // type: "required"
+  // },
+  {
+    emailAddress: {
+      address: "ronenshakes@yahoo.com",
+      name: "Ronen LeRokh",
+    },
+    type: "required",
+  },
 ];
 
 //CALL WITH THE ADD BUTTON FOR DATALIST
-function addAttendee(){
+function addAttendee() {
   selectedName = datalistValue.value;
   console.log(selectedName);
   attendee = {
-    emailAddress : {
-      address : namesAndEmails[selectedName],
-      name : selectedName
+    emailAddress: {
+      address: namesAndEmails[selectedName],
+      name: selectedName,
     },
-    type : "required"
-  }
+    type: "required",
+  };
   attendeesForEvent.push(attendee);
-  console.log("Attendee added! Name: " + selectedName + " Email: " + namesAndEmails[selectedName]);
-  
-  
+  console.log(
+    "Attendee added! Name: " +
+      selectedName +
+      " Email: " +
+      namesAndEmails[selectedName]
+  );
 }
 
 //key value pairs of names and emails of the contacts
 var namesAndEmails = {
   // "vembu":"kwvem@mails",
   // "ronen" : "ronen@mail"
-}
+};
 
 //create key-value pairs to store names and emails
-//this populates namesAndEmails 
-function processResponse(){
-  for (let i = 0; i < contactResponse.value.length; i++){
-    key = contactResponse.value[i].displayName
+//this populates namesAndEmails
+function processResponse() {
+  for (let i = 0; i < contactResponse.value.length; i++) {
+    key = contactResponse.value[i].displayName;
     value = contactResponse.value[i].emailAddresses[0].name;
-    Object.assign(namesAndEmails, {[key]: value});
-  } 
+    Object.assign(namesAndEmails, { [key]: value });
+  }
   console.log(namesAndEmails);
 }
 
 //used to call seeProfile()
-function callMSGraph(theUrl, accessToken){
+function callMSGraph(theUrl, accessToken) {
   var xmlHttp = new XMLHttpRequest();
-  xmlHttp.onreadystatechange = function(){
-    if(this.readyState == 4 && this.status == 200){
-      return(xmlHttp.responseText);
+  xmlHttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      return xmlHttp.responseText;
     }
-  }
+  };
   xmlHttp.open("GET", theUrl, true);
-  xmlHttp.setRequestHeader('Authorization','Bearer ' + accessToken);
+  xmlHttp.setRequestHeader("Authorization", "Bearer " + accessToken);
   xmlHttp.send();
 }
 
 //calls API to get list of user's contacts
-function callMSGraphPeople(theUrl, accessToken){
+function callMSGraphPeople(theUrl, accessToken) {
   var xmlHttp = new XMLHttpRequest();
-  xmlHttp.onreadystatechange = function(){
-    if(this.readyState == 4 && this.status == 200){
+  xmlHttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
       console.log(xmlHttp.response);
       console.log(typeof JSON.parse(xmlHttp.response));
       contactResponse = JSON.parse(xmlHttp.response);
       datalistEntry();
       return JSON.parse(xmlHttp.response);
     }
-  }
-  xmlHttp.open("GET",theUrl,true);
-  xmlHttp.setRequestHeader('Authorization', 'Bearer ' + accessToken);
-  xmlHttp.setRequestHeader('Content-Type', 'application/json');
+  };
+  xmlHttp.open("GET", theUrl, true);
+  xmlHttp.setRequestHeader("Authorization", "Bearer " + accessToken);
+  xmlHttp.setRequestHeader("Content-Type", "application/json");
   xmlHttp.send();
-
-
 }
-
 
 //Function that sends the event based on the reminder settings
 function submitButton(theUrl, token) {
@@ -183,13 +181,11 @@ function submitButton(theUrl, token) {
     // viewCalendar();
   }
 
-  callMSGraph2(theUrl,token);
-
+  callMSGraph2(theUrl, token);
 }
 
-//used to create calendar events 
+//used to create calendar events
 function callMSGraph2(theUrl, token) {
-  
   var xmlHttp = new XMLHttpRequest();
   xmlHttp.onreadystatechange = function () {
     if (this.readystate == 4 && this.status == 201) {
@@ -197,18 +193,13 @@ function callMSGraph2(theUrl, token) {
     }
   };
 
-
-
   xmlHttp.open("POST", theUrl, true);
   xmlHttp.setRequestHeader("Authorization", "Bearer " + token);
   xmlHttp.setRequestHeader("Content-Type", "application/json");
   xmlHttp.send(JSON.stringify(event));
-
-
-
 }
 
-function lookResponse(){
+function lookResponse() {
   console.log(contactResponse);
 }
 
@@ -220,10 +211,11 @@ function datalistEntry() {
   }
   for (let i = 0; i < contactResponse.value.length; i++) {
     var opt = document.createElement("option");
-    opt.appendChild(document.createTextNode(contactResponse.value[i].displayName));
+    opt.appendChild(
+      document.createTextNode(contactResponse.value[i].displayName)
+    );
     opt.value = contactResponse.value[i].displayName;
     datalist.appendChild(opt);
   }
   processResponse();
 }
-
