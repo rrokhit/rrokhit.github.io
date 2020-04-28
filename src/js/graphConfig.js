9a3f55a6a061c4c9c621dfae619ed4f8d108c72a
Ronen Rokhit <39106199+rrokhit@users.noreply.github.com>
2020-04-27 22:15:11 -0400

    Add files via upload

---
+++ src/js/graphConfig.js
@@ -0,0 +1,222 @@
+var subjectholder = document.getElementById("event-subject");
+var clientName = document.getElementById("client-name");
+var datalist = document.getElementById("attendees");
+var datalistValue = document.getElementById("attendees-search");
+var body = document.getElementById("event-content");
+var contactResponse;
+
+//subject line for calendar event
+clientName.addEventListener("input", function (event) {
+  selIndex = docType.selectedIndex;
+  console.log(docType);
+  console.log(clientName.value);
+  var subjectText = clientName.value + " " + docType;
+  subjectholder.value = subjectText;
+});
+
+//endpoints for various API calls
+const graphConfig = {
+  graphMeEndpoint: "https://graph.microsoft.com/v1.0/me",
+  graphMailEndpoint: "https://graph.microsoft.com/v1.0/me/messages",
+  graphCalendarEndpointOne:
+    "https://graph.microsoft.com/v1.0/me/calendar/events",
+  graphPeopleEndpoint: "https://graph.microsoft.com/v1.0/me/people",
+  graphContactsEndpoint:
+    "https://graph.microsoft.com/v1.0/me/contacts?$select = displayName,emailAddresses",
+};
+
+//event template
+const event = {
+  subject: subjectholder.value,
+  reminderMinutesBeforeStart: "",
+  body: {
+    contentType: "HTML",
+    content: body.value,
+  },
+  start: {
+    dateTime: "2020-05-15T12:00:00",
+    timeZone: "Eastern Standard Time",
+  },
+  end: {
+    dateTime: "2020-05-15T14:00:00",
+    timeZone: "Eastern Standard Time",
+  },
+  location: {
+    displayName: null,
+  },
+  attendees: [
+    {
+      emailAddress: {
+        address: "kwvembu@gmail.com",
+        name: "Vembushri RM",
+      },
+      type: "required",
+    },
+  ],
+};
+
+//append attendees to this array and then set the event attendees to this value
+attendeesForEvent = [
+  //this is the default attendee for each event
+  //   {emailAddress: {
+  //   address : "cdeslosges@desloges.ca",
+  //   name: "Chantal Desloges"
+  // },
+  // type: "required"
+  // },
+  {
+    emailAddress: {
+      address: "ronenshakes@yahoo.com",
+      name: "Ronen LeRokh",
+    },
+    type: "required",
+  },
+];
+
+//CALL WITH THE ADD BUTTON FOR DATALIST
+function addAttendee() {
+  selectedName = datalistValue.value;
+  console.log(selectedName);
+  attendee = {
+    emailAddress: {
+      address: namesAndEmails[selectedName],
+      name: selectedName,
+    },
+    type: "required",
+  };
+  attendeesForEvent.push(attendee);
+  console.log(
+    "Attendee added! Name: " +
+      selectedName +
+      " Email: " +
+      namesAndEmails[selectedName]
+  );
+}
+
+//key value pairs of names and emails of the contacts
+var namesAndEmails = {
+  // "vembu":"kwvem@mails",
+  // "ronen" : "ronen@mail"
+};
+
+//create key-value pairs to store names and emails
+//this populates namesAndEmails
+function processResponse() {
+  for (let i = 0; i < contactResponse.value.length; i++) {
+    key = contactResponse.value[i].displayName;
+    value = contactResponse.value[i].emailAddresses[0].name;
+    Object.assign(namesAndEmails, { [key]: value });
+  }
+  console.log(namesAndEmails);
+}
+
+//used to call seeProfile()
+function callMSGraph(theUrl, accessToken) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      return xmlHttp.responseText;
+    }
+  };
+  xmlHttp.open("GET", theUrl, true);
+  xmlHttp.setRequestHeader("Authorization", "Bearer " + accessToken);
+  xmlHttp.send();
+}
+
+//calls API to get list of user's contacts
+function callMSGraphPeople(theUrl, accessToken) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log(xmlHttp.response);
+      console.log(typeof JSON.parse(xmlHttp.response));
+      contactResponse = JSON.parse(xmlHttp.response);
+      datalistEntry();
+      return JSON.parse(xmlHttp.response);
+    }
+  };
+  xmlHttp.open("GET", theUrl, true);
+  xmlHttp.setRequestHeader("Authorization", "Bearer " + accessToken);
+  xmlHttp.setRequestHeader("Content-Type", "application/json");
+  xmlHttp.send();
+}
+
+//Function that sends the event based on the reminder settings
+function submitButton(theUrl, token) {
+  event.subject = subjectholder.value;
+  event.body.content = body.value;
+  event.attendees = attendeesForEvent;
+  event.start.dateTime = finalString;
+  event.end.dateTime = finalString;
+  console.log(event);
+  swal("Event Added!","Come back soon!", "success");
+
+  if (check("2-days")) {
+    // event.start.dateTime = realDateCalculation(-2, subDateString);
+    // event.end.dateTime = realDateCalculation(-2, subDateString);
+    event.isReminderOn = true;
+    event.reminderMinutesBeforeStart = 2880;
+
+    // viewCalendar();
+  }
+  if (check("7-days")) {
+    // event.start.dateTime = realDateCalculation(-7, subDateString);
+    // event.end.dateTime = realDateCalculation(-7, subDateString);
+    event.isReminderOn = true;
+    event.reminderMinutesBeforeStart = 10080;
+    // viewCalendar();
+  }
+  if (check("14-days")) {
+    // event.start.dateTime = realDateCalculation(-14, subDateString);
+    // event.end.dateTime = realDateCalculation(-14, subDateString);
+    event.isReminderOn = true;
+    event.reminderMinutesBeforeStart = 20160;
+    // viewCalendar();
+  }
+
+  if (check("21-days")) {
+    // event.start.dateTime = realDateCalculation(-21, subDateString);
+    // event.end.dateTime = realDateCalculation(-21, subDateString);
+    event.isReminderOn = true;
+    event.reminderMinutesBeforeStart = 30240;
+    // viewCalendar();
+  }
+
+  callMSGraph2(theUrl, token);
+}
+
+//used to create calendar events
+function callMSGraph2(theUrl, token) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function () {
+    if (this.readystate == 4 && this.status == 201) {
+      console.log(xmlHttp.responseText);
+    }
+  };
+
+  xmlHttp.open("POST", theUrl, true);
+  xmlHttp.setRequestHeader("Authorization", "Bearer " + token);
+  xmlHttp.setRequestHeader("Content-Type", "application/json");
+  xmlHttp.send(JSON.stringify(event));
+}
+
+function lookResponse() {
+  console.log(contactResponse);
+}
+
+//adds user's contacts to datalist options
+function datalistEntry() {
+  var len = datalist.length;
+  for (let i = 0; i < len; i++) {
+    documents.removeChild(datalist.options[0]);
+  }
+  for (let i = 0; i < contactResponse.value.length; i++) {
+    var opt = document.createElement("option");
+    opt.appendChild(
+      document.createTextNode(contactResponse.value[i].displayName)
+    );
+    opt.value = contactResponse.value[i].displayName;
+    datalist.appendChild(opt);
+  }
+  processResponse();
+}
---
+++ src/js/index.js
@@ -0,0 +1,274 @@
+//Guap Software
+var inputvalue = document.getElementById("date-selector");
+var radioResponse = document.getElementsByName("initial-menu");
+var isChecked = document.querySelector("input[name=initial-menu]:checked");
+var documents = document.getElementById("documents");
+var dateName = document.getElementById("date-name");
+var date;
+var docType;
+var finalString;
+var subDate = document.getElementById("due-date");
+var subDateString;
+
+
+//Document list for IRB documents
+documentListIRB = [
+  "Please Select:",
+  "Disclosure (IAD)",
+  "Notice of Appeal (IAD)",
+  "Disclosure (RPD)",
+  "Notice of Appeal (RAD)",
+  "Appeal Record",
+];
+
+//Document list for FC documents
+documentListFC = [
+  "Please Select:",
+  "Application for Leave and Judicial Review (Domestic)",
+  "Application for Leave and Judicial Review (International)",
+  "Applicant's Record",
+  "Affidavits and Memorandum",
+  "Reply Memorandum",
+  "Book of Authorities",
+];
+
+//Gets date from date box
+function getDate() {
+  var dateControl = document.querySelector('input[type="date"]');
+  date = dateControl.value;
+  console.log(date);
+}
+
+//Shows FC documents in drop-down menu
+function documentsFC() {
+  var len = documents.length;
+  for (let i = 0; i < len; i++) {
+    documents.removeChild(documents.options[0]);
+  }
+  for (let i = 0; i < documentListFC.length; i++) {
+    var opt = document.createElement("option");
+    opt.appendChild(document.createTextNode(documentListFC[i]));
+    opt.value = documentListFC[i];
+    documents.appendChild(opt);
+  }
+}
+
+//Shows IRB documents in drop-down menu
+function documentsIRB() {
+  var len = documents.length;
+  for (let i = 0; i < len; i++) {
+    documents.removeChild(documents.options[0]);
+  }
+
+  for (let i = 0; i < documentListIRB.length; i++) {
+    var opt = document.createElement("option");
+    opt.appendChild(document.createTextNode(documentListIRB[i]));
+    opt.value = documentListIRB[i];
+    documents.appendChild(opt);
+  }
+}
+
+//Dynamically changes drop-down menu to IRB or FC documents
+for (i = 0; i < radioResponse.length; i++) {
+  radioResponse[i].addEventListener("change", function (event) {
+    if (this.value == "IRB") {
+      documentsIRB();
+    } else {
+      documentsFC();
+    }
+  });
+}
+
+//what to do if docType = something
+function disclosure() {
+  dateName.innerHTML = "Date of hearing: ";
+}
+
+function noticeOfAppeal() {
+  dateName.innerHTML = "Date refusal received: ";
+}
+
+function applicationFLJR() {
+  dateName.innerHTML = "Date refusal received ";
+}
+
+function applicationRecordNoNine() {
+  dateName.innerHTML = "Date AFLJR sent: ";
+}
+
+function applicationRecordWithNine() {
+  dateName.innerHTML = "Date Rule 9 received: ";
+}
+
+function replyMemo() {
+  dateName.innerHTML = "Date FC memo received: ";
+}
+
+function appealRecord() {
+  dateName.innerHTML = "Date notice of appeal was filed: ";
+}
+
+//Saves value of DocType
+documents.addEventListener("change", function (event) {
+  docType = event.target.value;
+  console.log(docType);
+  if (docType == "Disclosure (IAD)" || docType == "Disclosure (RPD)") {
+    console.log(docType);
+    disclosure();
+  } else if (
+    docType == "Notice of Appeal (IAD)" ||
+    docType == "Notice of Appeal (RAD)"
+  ) {
+    noticeOfAppeal();
+  } else if (
+    docType == "Application for Leave and Judicial Review (Domestic)" ||
+    docType == "Application for Leave and Judicial Review (International)"
+  ) {
+    applicationFLJR();
+  } else if (docType == "Applicant's Record (No Rule 9)") {
+    applicationRecordNoNine();
+  } else if (docType == "Applicant's Record (With Rule 9)") {
+    applicationRecordWithNine();
+  } else if (
+    docType == "Reply Memorandum" ||
+    docType == "Affidavits and Memorandum"
+  ) {
+    replyMemo();
+  } else if (docType == "Appeal Record") {
+    appealRecord();
+  }
+});
+
+function check(id) {
+  return document.getElementById(id).checked;
+}
+
+inputvalue.addEventListener("change", function (event) {
+  date = inputvalue.value;
+  //date = date.split("-");
+  //dateConvert();
+  console.log(date);
+  dateCalculation();
+  // document.getElementById("date-form").submit();
+});
+
+function addDays(date, days) {
+  var result = new Date(date);
+  result.setDate(result.getDate() + days);
+  return result;
+}
+
+function dateConvert(string) {
+  string = string.split("-");
+  for (let x = 0; x < string.length; x++) {
+    if (string[x] < 10) {
+      string[x] = "0" + string[x];
+    }
+    string[x] = "" + string[x];
+  }
+  string = string.join("-");
+  return string;
+}
+
+//dateTime: "2020-05-15T12:00:00",
+//20 days = 1728000000 milliseconds
+//30 days = 2592000000 milliseconds
+function dateCalculation() {
+  if (docType == "Disclosure (IAD)") {
+    //for disclosure IAD, it is due 20 days before hearing
+    //inputDate is the date of hearing
+    finalString = realDateCalculation(-20, date);
+    console.log(finalString);
+  } else if (docType == "Notice of Appeal (IAD)") {
+    //notice of appeal must be filed 30 days after refusal
+    //input date is date of refusal
+    finalString = realDateCalculation(30, date);
+    console.log(finalString);
+  } else if (docType == "Disclosure (RPD)") {
+    //disclosure RPD is due 10 days before hearing
+    //input date is date of hearing
+    finalString = realDateCalculation(-10, date);
+    console.log(finalString);
+  } else if (docType == "Notice of Appeal (RAD)") {
+    //notice of appeal RAD must be filed 15 days after refusal
+    //input date is date of refusal
+    finalString = realDateCalculation(15, date);
+    console.log(finalString);
+  } else if (docType == "Appeal Record") {
+    //idk what the appeal record is tbh
+    //i think input date is date of refusal
+    finalString = realDateCalculation(180, date);
+    console.log(finalString);
+  } else if (
+    docType == "Application for Leave and Judicial Review (Domestic)"
+  ) {
+    //AFLJR(domestic) is due 15 days after refusal
+    //input date is date of refusal
+    finalString = realDateCalculation(15, date);
+    console.log(finalString);
+  } else if (
+    docType == "Application for Leave and Judicial Review (International)"
+  ) {
+    //AFLJR (intl) is due 60 days after refusal
+    //input date is date of refusal
+    finalString = realDateCalculation(60, date);
+    console.log(finalString);
+  } else if (docType == "Applicant's Record (No Rule 9)") {
+    //Applicants record is due 30 days after AFLJR is sent
+    //input date is date AFLJR sent
+    finalString = realDateCalculation(30, date);
+    console.log(finalString);
+  } else if (docType == "Applicant's Record (With Rule 9)") {
+    //Applicant's record is due 40 days after rule 9 received
+    //input date is date rule 9 received
+    finalString = realDateCalculation(40, date);
+    console.log(finalString);
+  } else if (docType == "Reply Memorandum") {
+    //reply memo is sent 10 days after FC memo
+    // input date is when FC memo is received
+    finalString = realDateCalculation(10, date);
+    console.log(finalString);
+  }
+  subDate.value = dateConvert(subDateString);
+}
+
+function realDateCalculation(interval, funcDate) {
+  var inputDate = new Date(funcDate);
+  inputDate.setDate(inputDate.getDate() + 1);
+  console.log(inputDate);
+  var outputDate = addDays(inputDate, interval);
+  console.log(outputDate);
+  if (outputDate.getDay() == 0) {
+    outputDate = addDays(outputDate, 1);
+  } else if (outputDate.getDay() == 6) {
+    outputDate = addDays(outputDate, -1);
+  }
+  console.log("after checking fo weekend: " + outputDate);
+  subDateString =
+    outputDate.getFullYear() +
+    "-" +
+    (outputDate.getMonth() + 1) +
+    "-" +
+    outputDate.getDate();
+  finalString = subDateString + "T12:00:00";
+  return finalString;
+}
+
+// date : "2020-03-14"
+valuesIRB = {
+  "Disclosure (IAD)": 20,
+  "Notice of Appeal (IAD)": 30,
+  "Disclosure (RPD)": 10,
+  "Notice of Appeal (RAD)": 15,
+};
+
+documentListFC = [
+  "Please Select:",
+  "Application for Leave and Judicial Review (Domestic)",
+  "Application for Leave and Judicial Review (International)",
+  "Applicant's Record (No Rule 9)",
+  "Applicant's Record (With Rule 9)",
+  "Affidavits and Memorandum",
+  "Reply Memorandum",
+  // "Book of Authorities"
+];
